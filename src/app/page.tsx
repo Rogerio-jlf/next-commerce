e8dfb34776bc78aeba8a3db5edf7f1373dba6fa9
rogerio.jlf <rogerio.jlf@gmail.com>
2025-01-14 16:41:42 -0300

    Adicionando loading para a página inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,33 @@
+import Stripe from "stripe";
 import { ProductType } from "../types/ProductType";
 import Product from "./components/Product";
 
-async function getProducts() {
-  const response = await fetch("https://fakestoreapi.com/products");
-  if (!response.ok) {
-    throw new Error("Erro ao buscar produtos");
+async function getProducts(): Promise<ProductType[]> {
+  if (!process.env.STRIPE_SECRET_KEY) {
+    throw new Error("Stripe secret key is not defined");
   }
-  return response.json();
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: "2024-12-18.acacia",
+  });
+
+  const products = await stripe.products.list();
+  const formattedProducts = await Promise.all(
+    products.data.map(async (product) => {
+      const price = await stripe.prices.list({
+        product: product.id,
+      });
+      return {
+        id: product.id,
+        name: product.name,
+        image: product.images[0],
+        description: product.description,
+        price: price.data[0].unit_amount,
+        currency: price.data[0].currency,
+      };
+    })
+  );
+
+  return formattedProducts;
 }
 
 export default async function Home() {
@@ -15,7 +36,7 @@ export default async function Home() {
   return (
     <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
-        {products.map((product: ProductType) => (
+        {products.map((product) => (
           <Product key={product.id} product={product}></Product>
         ))}
       </div>
