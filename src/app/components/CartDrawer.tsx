f097b460c76af313c5345392d8e0dde62aa67d30
rogerio.jlf <rogerio.jlf@gmail.com>
2025-01-16 10:43:33 -0300

    Atualizando últimas alterações

--- src/app/components/CartDrawer.tsx
+++ src/app/components/CartDrawer.tsx
@@ -4,6 +4,7 @@ import { formatPrice } from "@/lib/utils";
 import { useCartStore } from "@/store";
 import Image from "next/image";
 import CheckoutButton from "./CheckoutButton";
+import Checkout from "./Chekout";
 
 export default function CartDrawer() {
   const useStore = useCartStore();
@@ -27,41 +28,45 @@ export default function CartDrawer() {
         >
           Voltar para loja
         </button>
-        <div className="border-t border-gray-400 my-4">
-          {useStore.cart.map((item) => (
-            <div className="flex gap-4 py-4" key={item.id}>
-              <Image
-                className="object-cover w-24"
-                src={item.image}
-                alt={item.name}
-                width={120}
-                height={120}
-              />
-              <div>
-                <h2 className="w-42 truncate">{item.name}</h2>
-                <h2>Quantidade: {item.quantity}</h2>
-                <p className="text-teal-600 text-sm font-bold">
-                  {formatPrice(item.price)}
-                </p>
-                <button
-                  className="py-1 px-2 border rounded-md mt-2 text-sm mr-1"
-                  onClick={() => useStore.addProduct(item)}
-                >
-                  Adicinar
-                </button>
-                <button
-                  className="py-1 px-2 border rounded-md mt-2 text-sm"
-                  onClick={() => useStore.removeProduct(item)}
-                >
-                  Remover
-                </button>
+        <div className="border-t border-gray-400 my-4"></div>
+        {useStore.onCheckout === "cart" && (
+          <>
+            {useStore.cart.map((item) => (
+              <div className="flex gap-4 py-4" key={item.id}>
+                <Image
+                  className="object-cover w-24"
+                  src={item.image}
+                  alt={item.name}
+                  width={120}
+                  height={120}
+                />
+                <div>
+                  <h2 className="w-42 truncate">{item.name}</h2>
+                  <h2>Quantidade: {item.quantity}</h2>
+                  <p className="text-teal-600 text-sm font-bold">
+                    {formatPrice(item.price)}
+                  </p>
+                  <button
+                    className="py-1 px-2 border rounded-md mt-2 text-sm mr-1"
+                    onClick={() => useStore.addProduct(item)}
+                  >
+                    Adicinar
+                  </button>
+                  <button
+                    className="py-1 px-2 border rounded-md mt-2 text-sm"
+                    onClick={() => useStore.removeProduct(item)}
+                  >
+                    Remover
+                  </button>
+                </div>
               </div>
-            </div>
-          ))}
-          {useStore.cart.length > 0 && useStore.onCheckout === "cart" && (
-            <CheckoutButton totalPrice={totalPrice} />
-          )}
-        </div>
+            ))}
+          </>
+        )}
+        {useStore.cart.length > 0 && useStore.onCheckout === "cart" && (
+          <CheckoutButton totalPrice={totalPrice} />
+        )}
+        {useStore.onCheckout === "checkout" && <Checkout />}
       </div>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,13 @@
-import Stripe from "stripe";
-import { ProductType } from "../types/ProductType";
-import Product from "./components/Product";
-
-async function getProducts(): Promise<ProductType[]> {
-  if (!process.env.STRIPE_SECRET_KEY) {
-    throw new Error("Stripe secret key is not defined");
-  }
-  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-    apiVersion: "2024-12-18.acacia",
-  });
-
-  const products = await stripe.products.list();
-  const formattedProducts = await Promise.all(
-    products.data.map(async (product) => {
-      const price = await stripe.prices.list({
-        product: product.id,
-      });
-      return {
-        id: product.id,
-        name: product.name,
-        image: product.images[0],
-        description: product.description,
-        price: price.data[0].unit_amount,
-        currency: price.data[0].currency,
-      };
-    })
-  );
-
-  return formattedProducts;
-}
+import { fetchProducts } from "./actions";
+import InfiniteScroll from "./components/InfiniteScroll";
 
 export default async function Home() {
-  const products = await getProducts();
+  const { formatedProducts } = await fetchProducts({});
 
   return (
     <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
-        {products.map((product) => (
-          <Product key={product.id} product={product}></Product>
-        ))}
+        <InfiniteScroll initialProducts={formatedProducts} />
       </div>
     </div>
   );
